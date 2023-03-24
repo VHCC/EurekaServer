463de69dfe7e0235ce3cc407a12dbed6c21bb369
ichenSSD\AiCS <r99521320@gmail.com>
2023-03-24 14:10:05 +0800

    validate in conventional Eureka

--- README.md
+++ README.md
@@ -11,3 +11,15 @@ proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
 proxy_set_header X-Forwarded-Proto $scheme;
 proxy_pass http://127.0.0.1:8761/eureka-server;
 }
+
+
+## noted
+Spring Cloud并不是停止维护，而是从2020年开始转向社区驱动的开发模式。这意味着它将不再由Pivotal公司负责维护，而是由社区成员、贡献者和企业共同负责维护。该项目仍然是活跃的，并且仍然有很多人在使用和开发它。
+
+
+注册中心：Nacos、Eureka、Zookeeper、Consul
+服务调用：LoadBalancer、Ribbon、OpenFeign（Feign）
+服务熔断：Sentinel、Resilience4j、Hystrix
+服务网关：Gateway、Zuul
+服务配置：Nacos、Config
+服务总线：Nacos、Bus
--- src/main/java/com/frank/spring/eureka/server/ServerApplication.java
+++ src/main/java/com/frank/spring/eureka/server/ServerApplication.java
@@ -1,16 +1,16 @@
 package com.frank.spring.eureka.server;
 
-import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableAdminServer
+@EnableEurekaServer
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class ServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
 
 }
